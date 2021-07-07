bc8daa17ffd0130031404a3ffba731c94aa900fd
Kchugames <85512384+Kchugames@users.noreply.github.com>
2021-07-07 17:01:42 +0100

    Add files via upload

---
+++ MCP1.js
@@ -0,0 +1,55 @@
+
+
+function showAlert() {
+    var Response = "Were you surprised?";
+    prompt (Response);
+        // if Response = "yes";
+    
+}
+
+function picture(){ 
+    var pic = "https://img.icons8.com/ios/50/000000/ghost--v1.png"; alt="cute white ghost button";
+    document.getElementById('blackgh').src = pic;
+    document.getElementById('blackgh').style.width="450px";
+    document.getElementById('blackgh').style.height="350px";   
+    }
+
+
+
+function colourChange() {
+document.getElementById('colch') .style.backgroundColor =("tomato");
+document.getElementById('Image1') .style.display ="none"
+}
+
+function bigImg(x) {
+x.style.height = "1700px";
+x.style.width = "1250px";
+}
+
+function normalImg(x) {
+x.style.height = "750px";
+x.style.width = "600px";
+}
+
+
+
+function Buprom() {
+	var response = prompt("Were you Spooked?");
+	if (response == "yes") {
+		alert("Don't worry! it's just a website!");
+	}
+	else if (response == "no") {
+		alert("You must be very brave!");
+	}
+	else {
+		alert("I don't have a clue what you said!");
+	}
+}
+
+
+
+
+
+// https://www.kindpng.com/imgv/JhwRJh_cute-ghost-png-cute-ghost-pixel-art-transparent/
+
+// https://img.icons8.com/ios/50/000000/ghost--v1.png
